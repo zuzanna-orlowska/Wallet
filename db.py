d98710ea6d0235834bf482a947c51e34b8eeac59
zuzia <orlowska.zu@gmail.com>
2022-06-09 18:10:30 +0200

    refactor

--- db.py
+++ db.py
@@ -1,5 +1,6 @@
 import sqlite3
-
+from datetime import date
+import config
 
 
 def dbInit():
@@ -29,17 +30,25 @@ def dbCreateTables():
         foreign key(Kat_ID) references Kategoria(ID))""")
 
 def dbInitInsert():
-    tasks = [("Opłaty konieczne",), ("Oszczędności",), ("Przyjemności",), ("Hobby",), ("Inwestycje",), ("Dla innych",)]
     sql = '''insert into Kategoria (Nazwa) values(?)'''
 
-    for task in tasks:
-        cur.execute(sql, task)
+    for name in config.categoryNames:
+        cur.execute(sql, name)
     con.commit()
 
 def dbIncomeSplit(income):
     distributionPercentage = [0.35, 0.15, 0.3, 0.05, 0.1, 0.05]
     distributedIncome = []
 
-    for item in distributionPercentage:
-        distributedIncome.append(round(income*item, 2))
+    for index, item in enumerate(distributionPercentage):
+        distributedIncome.append([index+1, round(income*item, 2), str(date.today())])
+
+    dbInsertBudget(distributedIncome)
+
+def dbInsertBudget(distributedIncomes):
+    sql = '''insert into Budzet (Kat_ID, Kwota, Data) values(?, ?, ?)'''
+
+    for income in distributedIncomes:
+        cur.execute(sql, income)
+    con.commit()
 
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import sqlite3
-
 import db
+import gui
 
-# db.dbInit()
-#
+db.dbInit()
+
 # db.dbCreateTables()
 #
 # db.dbInitInsert()
 
 db.dbIncomeSplit(2001)
+
+gui.guiInit()
